bee7b1f1c4f074f0c4c8d3b1d50305cb882cdb66
João Victor Tavares <joaojv497@hotmail.com>
2021-08-08 20:32:54 -0300

    Correção socket.io

--- index.js
+++ index.js
@@ -21,16 +21,24 @@ app.get('/', (req, res, next) => {
 
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
+    socket.on("led", (arg) => {
+        port.write(`${arg}`, function(err) {
+            if (err) {
+                return console.log('Error on write: ', err.message);
+            }
+        });
+        // console.log(arg);
+    });
 })
 
 port.pipe(parser);
 port.on('open', () => {
     console.log('Conexão bluetooth iniciada...');
-    port.write('2', function(err) {
-        if (err) {
-            return console.log('Error on write: ', err.message);
-        }
-    });
+    // port.write('1', function(err) {
+    //     if (err) {
+    //         return console.log('Error on write: ', err.message);
+    //     }
+    // });
     parser.on('data', (line) => {
         dadosArduino = line;
         console.log(line);
