938c711e3cc148f27ce8b18cd8e4315ccab7eedf
João Victor Tavares <joaojv497@hotmail.com>
2021-09-30 20:55:19 -0300

    Ajustes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ const router_login = require("./routes/login");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-//app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
--- src/connection.js
+++ src/connection.js
@@ -1,4 +1,3 @@
 const app = require("./app");
 const http = require("http").createServer(app);
-//app.use(bodyParser.urlencoded({ extended: true }));
 module.exports = http;
