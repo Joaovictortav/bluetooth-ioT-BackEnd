07f4c4f3206c644011fab17f69267b19c08835b3
Joaovictortav <joaojv497@hotmail.com>
2021-10-01 23:49:57 -0300

    Ajuste

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,14 +1,5 @@
 const loginService = require("../service/loginService");
-const authMidleware = require('../authMidleware');
 const router = require("express").Router();
-
-router.get("/", (req, res, next) => {
-    try {
-        res.status(200).send(`<h1>Hey Socket.io</h1>`);
-    } catch (error) {
-        console.log(error);
-    }
-});
 
 router.post("/", (req, res, next) => {
     try {
