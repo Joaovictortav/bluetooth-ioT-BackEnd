1073ba6af00d7dd2f22aeb4a3ba4b7f13c5c4147
Joaovictortav <joaojv497@hotmail.com>
2021-10-08 17:39:01 -0300

    Retirada do bluetooth

--- src/routes/socket.js
+++ src/routes/socket.js
@@ -1,28 +1,10 @@
-const { port, parser } = require("../config/configBluetooth");
 const io = require("../config/configSocket");
-
-let dadosArduino;
-
-port.on('open', () => {
-
-    console.log('Conexão bluetooth iniciada...');
-
-    parser.on('data', (line) => {
-        dadosArduino = line;
-        // console.log(line);
-    });
-
-});
 
 io.on("connection", (socket) => {
     console.log(`Socket conectado: ${socket.id}`);
 
     socket.on("led", (arg) => {
-        port.write(`${arg}`, function(err) {
-            if (err) {
-                return console.log("Error: ", err.message);
-            }
-        });
+
     });
 
 });
