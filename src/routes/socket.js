a1d52722b834b85639afe511b6ea6740453f2c66
Joaovictortav <joaojv497@hotmail.com>
2021-10-11 01:05:44 -0300

    Ajustes

--- src/routes/socket.js
+++ src/routes/socket.js
@@ -7,6 +7,7 @@ const client_mqtt_1 = mqtt('mqtt_1');
 io.on("connection", (socket) => {
     console.log(`Socket conectado: ${socket.id}`);
 
+
     socket.on("led", (arg) => {
         controllerLed(arg, client_mqtt_1);
     });
